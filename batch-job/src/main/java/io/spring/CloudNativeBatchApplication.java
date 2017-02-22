6fc69b71c74fc3c5de593121a922e3531f05bc5d
Michael Minella <mminella@pivotal.io>
2017-02-21 23:19:17 -0600

    Spring Cloud components added - Rework application.yml and boostrap.yml

--- batch-job/src/main/java/io/spring/CloudNativeBatchApplication.java
+++ batch-job/src/main/java/io/spring/CloudNativeBatchApplication.java
@@ -3,11 +3,13 @@ package io.spring;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.retry.annotation.EnableRetry;
 
 @EnableRetry
 @EnableBatchProcessing
 @SpringBootApplication
+@EnableDiscoveryClient(autoRegister = false)
 public class CloudNativeBatchApplication {
 
 	public static void main(String[] args) {
